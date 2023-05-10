0d6f5df94c036a444a30f023d654b1dff9b8734a
12-kavya <vkavya234@gmail.com>
2023-05-10 14:57:46 +0530

    first commit

---
+++ src/main/java/com/id/jwtauthentication/controllers/HomeController.java
@@ -0,0 +1,24 @@
+package com.id.jwtauthentication.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	
+	
+	@RequestMapping("/welcome")
+	public String welcome(){
+		String text="This is private page.";
+		text+="This page is not allowed to unauthenticated users";
+		return text;	
+	}
+	
+	@RequestMapping("/getusers")
+	public String getUser(){
+//		String text="This is private page.";
+//		text+="This page is not allowed to unauthenticated users";
+		return "{\"name\":\"Kavya\"}";	
+	}
+
+}
